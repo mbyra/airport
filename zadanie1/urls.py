d73285c53d264f0473bff38352951c3bcaf0417e
marcin <marbit12@gmail.com>
2018-05-13 13:24:09 +0200

    Initial commit, after 4 parts of django tutorial starting to convert it to my project

---
+++ zadanie1/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls import url
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
